6100f2f9e9135b369ea015082e88b46d1eb7e3e4
Ahmedkhalifa1999 <ahmedkhalifa1999@gmail.com>
2021-06-13 23:43:49 +0200

    Final Commit 6

--- Distance.c
+++ Distance.c
@@ -9,8 +9,5 @@ double distance(double lon1, double lon2, double lat1, double lat2)
 	double a = sin((lat2 - lat1) * PI / 360) * sin((lat2 - lat1) * PI / 360);
 	double b = sin((lon2 - lon1) * PI / 360) * sin((lon2 - lon1) * PI / 360);
 	distance = abs(2000 * R * asin(sqrt(a + cos(lat2 * PI / 180) * cos(lat1 * PI / 180) * b)));
-	if (distance < 0.6) return distance;
-	else return 0.6;
+	return distance;
 }
-
-
